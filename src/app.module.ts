7ccd0ae6160b35cb663fbb0a1243c52f1b67255d
Anton <anton.vasilenko00@gmail.com>
2022-07-02 13:16:59 +0300

    feat: add setup for mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { OkaiyaGameModule } from './okaiya-game/okaiya-game.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [OkaiyaGameModule],
+  imports: [OkaiyaGameModule, UsersModule, ConfigModule.forRoot(), MongooseModule.forRoot(`mongodb+srv://antonvasilenko:${process.env.MONGO_DB_USER_PASSWORD}@maincluster.g8rf6.mongodb.net/?retryWrites=true&w=majority`, {dbName: process.env.MONGO_DB_NAME })],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 
 async function bootstrap() {
+  console.log( process.env.MONGO_DB_NAME)
+
   const app = await NestFactory.create(AppModule);
+
+  
 
   app.enableCors();
 
