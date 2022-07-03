54c9e71d417599f348ab5a80dd79b542724de4fa
a.vasilenka <anton.vasilenko00@gmail.com>
2022-07-03 13:50:21 +0300

    feat: add jwt module to login route

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,9 +4,13 @@ import {AuthService} from './auth.service';
 import {UsersModule} from '../users/users.module'
 import {PassportModule} from '@nestjs/passport'
 import {LocalStrategy} from './local.strategy'
+import {JwtModule} from '@nestjs/jwt'
 
 @Module({
-  imports: [UsersModule, PassportModule],
+  imports: [UsersModule, PassportModule, JwtModule.register({
+    secret: process.env.JWT_SECRET || 'SUPER_SECRET_SECRET',
+    signOptions: { expiresIn: '24h' }
+  })],
   controllers: [AuthController],
   providers: [AuthService, LocalStrategy]
 })
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,10 +2,11 @@ import {ForbiddenException, Injectable, NotFoundException} from '@nestjs/common'
 import {UsersService} from '../users/users.service'
 import * as _ from 'lodash'
 import {User} from '../users/entities/user.entity'
+import {JwtService} from '@nestjs/jwt'
 
 @Injectable()
 export class AuthService {
-  constructor(private usersService: UsersService) {
+  constructor(private usersService: UsersService, private jwtService: JwtService) {
   }
 
   async validateUser(username: string, password: string): Promise<Partial<User> | false> {
@@ -18,5 +19,12 @@ export class AuthService {
     if (!isPasswordValid) throw new ForbiddenException('Invalid Password')
 
     return _.omit(user, 'password');
+  }
+
+  async login(user: User) {
+    const payload = { username: user.userName, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload)
+    };
   }
 }
