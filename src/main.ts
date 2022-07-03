d3560a878a3ddcf9e7ed2c162cdb2e7052403a91
a.vasilenka <anton.vasilenko00@gmail.com>
2022-07-03 14:44:58 +0300

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { WsAdapter } from '@nestjs/platform-ws';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -9,6 +10,17 @@ async function bootstrap() {
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true }));
   app.useWebSocketAdapter(new WsAdapter(app));
+
+  const config = new DocumentBuilder()
+    .setTitle('AV-services')
+    .setDescription('AV-services API description')
+    .setVersion('1.0')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(process.env.PORT || 3000);
 }
