b216193150f2d0680d22d05b8981b67baaeeb485
a.vasilenka <anton.vasilenko00@gmail.com>
2022-07-03 16:04:03 +0300

    feat: add swagger for okaiya and root

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,10 +1,14 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('root')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Application Health-check endpoint' })
+  @ApiOkResponse()
   @Get('/')
   getServer(): string {
     return this.appService.getServer();
--- src/okaiya-game/okaiya-game.controller.ts
+++ src/okaiya-game/okaiya-game.controller.ts
@@ -1,11 +1,15 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('okaiya-game')
 @Controller('okaiya-game')
 export class OkaiyaGameController {
+  @ApiOperation({ summary: 'Health-check endpoint' })
+  @ApiOkResponse()
   @UseGuards(JwtAuthGuard)
   @Get('/')
   getIndex() {
-    return 'Okayia';
+    return 'Okayia!';
   }
 }
